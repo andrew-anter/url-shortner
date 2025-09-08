1badf84bdae144f022b93dbf621862fc97becdbf
Andrew Anter <andrew.anter@gmail.com>
2025-09-08 23:38:50 +0300

    created the hashing function to return the first 6 hexadecimal code
    this is the first scratch for the url shortner

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import hashlib
+
+url_dict = {}
+
+
 def main():
     print("Hello from url-shortner!")
     test_url = "https://www.google.com/search?q=project+bassed+learning+python+url+shortner&sca_esv=c2dc18171226bd7b&sxsrf=AE3TifMunHSGJmPica_IS4klW6UnbMhF-w%3A1757360544127&ei=oDG_aIjCB8GK7M8P9OONoQ0&ved=0ahUKEwiIkuaX9smPAxVBBfsDHfRxI9QQ4dUDCBA&uact=5&oq=project+bassed+learning+python+url+shortner&gs_lp=Egxnd3Mtd2l6LXNlcnAiK3Byb2plY3QgYmFzc2VkIGxlYXJuaW5nIHB5dGhvbiB1cmwgc2hvcnRuZXIyBxAhGKABGAoyBxAhGKABGApI-hJQEFjhEXAAeAKQAQCYAc8CoAHYE6oBBzAuMi41LjO4AQPIAQD4AQGYAgugAokUwgIEEAAYR8ICBxAAGIAEGA3CAgYQABgWGB7CAggQABgIGA0YHsICCxAAGIAEGIYDGIoFwgIIEAAYgAQYogTCAgUQABjvBZgDAOIDBRIBMSBAiAYBkAYIkgcHMS4yLjUuM6AHo0CyBwcwLjIuNS4zuAeEFMIHCTAuOC4yLjAuMcgHJg&sclient=gws-wiz-serp"
-    url_shortner(url=test_url)
+
+    print(url_shortner(url=test_url))
 
 
 def url_shortner(*, url: str):
-    print(url)
-    pass
+    url_utf8_encoded = url.encode("utf-8")
+    md5_hash_object = hashlib.md5()
+    md5_hash_object.update(url_utf8_encoded)
+
+    shortened_url_code = md5_hash_object.hexdigest()[:6]
+
+    if shortened_url_code not in url_dict:
+        url_dict[shortened_url_code] = url_utf8_encoded
+
+    return shortened_url_code
 
 
 if __name__ == "__main__":
