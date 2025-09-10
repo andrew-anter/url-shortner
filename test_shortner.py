3d3ec72c798664a9c83bd3f43ce0f121cdf4a34f
Andrew Anter <andrew.anter@gmail.com>
2025-09-10 22:51:00 +0300

    updated the tests to import and use the new function name

--- test_shortner.py
+++ test_shortner.py
@@ -1,10 +1,10 @@
-from functions import url_shortner
+from functions import url_shortner_md5
 
 
 def test_url_shortner():
     test_url1 = "www.google.com"
 
-    shortned_url1 = url_shortner(url=test_url1)
-    shortned_url2 = url_shortner(url=test_url1)
+    shortned_url1 = url_shortner_md5(url=test_url1)
+    shortned_url2 = url_shortner_md5(url=test_url1)
 
     assert shortned_url1 == shortned_url2
