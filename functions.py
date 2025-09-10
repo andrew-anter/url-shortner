404e0ab25391c275f6d1c4bc4c2f2b11f6b2d58c
Andrew Anter <andrew.anter@gmail.com>
2025-09-10 22:50:06 +0300

    updated url_shortner name and removed unnecessary variable

--- functions.py
+++ functions.py
@@ -6,16 +6,15 @@ protocol = "http"
 base_url = f"{protocol}://{base_domain}"
 
 
-def url_shortner(*, url: str):
+def url_shortner_md5(*, url: str) -> str:
     url_utf8_encoded = url.encode("utf-8")
     md5_hash_object = hashlib.md5()
     md5_hash_object.update(url_utf8_encoded)
 
     ## will work for 4B urls
     shortened_url_code = md5_hash_object.hexdigest()[:8]
-    url = f"{base_url}/{shortened_url_code}"
 
     if shortened_url_code not in url_dict:
         url_dict[shortened_url_code] = url_utf8_encoded
 
-    return url
+    return f"{base_url}/{shortened_url_code}"
