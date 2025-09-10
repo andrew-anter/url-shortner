1212785582c52ba013be8aa1888e47714a432d9d
Andrew Anter <andrew.anter@gmail.com>
2025-09-10 23:56:49 +0300

    created a second version of the url shortner that uses random combination of ascii letters and numbers

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import hashlib
+import random
+import string
 
 url_dict = {}
 base_domain = "localhost"
@@ -7,6 +9,9 @@ base_url = f"{protocol}://{base_domain}"
 
 
 def url_shortner_md5(*, url: str) -> str:
+    """
+    supports to 16^8 => 4B.
+    """
     url_utf8_encoded = url.encode("utf-8")
     md5_hash_object = hashlib.md5()
     md5_hash_object.update(url_utf8_encoded)
@@ -16,5 +21,25 @@ def url_shortner_md5(*, url: str) -> str:
 
     if shortened_url_code not in url_dict:
         url_dict[shortened_url_code] = url_utf8_encoded
+
+    return f"{base_url}/{shortened_url_code}"
+
+
+def url_shortner_alphanumeric(*, url: str) -> str:
+    """
+    supports up to 62 ^ 6 urls => 56B urls.
+    """
+    characters: list[str] = [character for character in string.ascii_letters] + [
+        str(number) for number in range(0, 10)
+    ]
+    url_utf8_encoded = url.encode("utf-8")
+
+    shortened_url_code = ""
+
+    if url_utf8_encoded in url_dict:
+        shortened_url_code = url_dict[url_utf8_encoded]
+    else:
+        shortened_url_code = "".join(random.choice(characters) for _ in range(6))
+        url_dict[url_utf8_encoded] = shortened_url_code
 
     return f"{base_url}/{shortened_url_code}"
--- test_shortner.py
+++ test_shortner.py
@@ -1,10 +1,19 @@
-from functions import url_shortner_md5
+from functions import url_shortner_md5, url_shortner_alphanumeric
 
 
-def test_url_shortner():
+def test_url_shortner_md5():
     test_url1 = "www.google.com"
 
     shortned_url1 = url_shortner_md5(url=test_url1)
     shortned_url2 = url_shortner_md5(url=test_url1)
+
+    assert shortned_url1 == shortned_url2
+
+
+def test_url_shortner_alphanumeric():
+    test_url1 = "www.google.com"
+
+    shortned_url1 = url_shortner_alphanumeric(url=test_url1)
+    shortned_url2 = url_shortner_alphanumeric(url=test_url1)
 
     assert shortned_url1 == shortned_url2
