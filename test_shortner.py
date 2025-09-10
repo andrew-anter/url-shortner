1c70e7302aba19b5d810fbc3c31ee457e128b1cf
Andrew Anter <andrew.anter@gmail.com>
2025-09-10 22:44:07 +0300

    removed the nothing test that was testing the testing environment is running

--- test_shortner.py
+++ test_shortner.py
@@ -1,15 +1,10 @@
 from functions import url_shortner
 
 
-def test_nothing():
-    assert True
-
-
 def test_url_shortner():
     test_url1 = "www.google.com"
-    test_url2 = "www.google.com"
 
     shortned_url1 = url_shortner(url=test_url1)
-    shortned_url2 = url_shortner(url=test_url2)
+    shortned_url2 = url_shortner(url=test_url1)
 
     assert shortned_url1 == shortned_url2
