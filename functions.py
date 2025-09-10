13fbfaa68335d734a54ad422f06ba45caa6caa66
Andrew Anter <andrew.anter@gmail.com>
2025-09-11 00:40:25 +0300

    created a new url shortner algorithm that depends on a counter and base conversion instead of randomness or hashing

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ URL_LEN = 8
 BASE_DOMAIN = "localhost"
 PROTOCOL = "http"
 BASE_URL = f"{PROTOCOL}://{BASE_DOMAIN}"
+counter = 1e6
 
 
 def url_shortner_md5(*, url: str) -> str:
@@ -18,7 +19,7 @@ def url_shortner_md5(*, url: str) -> str:
     md5_hash_object = hashlib.md5()
     md5_hash_object.update(url_utf8_encoded)
 
-    ## will work for 4B urls
+    # FIX: collission prune due to slicing
     shortened_url_code = md5_hash_object.hexdigest()[:URL_LEN]
 
     if shortened_url_code not in url_dict:
@@ -32,9 +33,8 @@ def url_shortner_alphanumeric(*, url: str) -> str:
     Supports 62 ^ URL_LEN
     in case of URL_LEN = 8 then it will support approximately 2*10 ^ 14
     """
-    characters: list[str] = [character for character in string.ascii_letters] + [
-        str(number) for number in range(0, 10)
-    ]
+    characters: str = string.ascii_letters + string.digits
+
     url_utf8_encoded = url.encode("utf-8")
 
     shortened_url_code = ""
@@ -42,7 +42,33 @@ def url_shortner_alphanumeric(*, url: str) -> str:
     if url_utf8_encoded in url_dict:
         shortened_url_code = url_dict[url_utf8_encoded]
     else:
+        # FIX: collision prune due to randomness collisisons
         shortened_url_code = "".join(random.choice(characters) for _ in range(URL_LEN))
         url_dict[url_utf8_encoded] = shortened_url_code
 
     return f"{BASE_URL}/{shortened_url_code}"
+
+
+def url_shortner_by_base_conversion(*, url: str) -> str:
+    if url in url_dict:
+        return f"{BASE_URL}/{url_dict[url]}"
+
+    BASE = 62
+    BASE62_CHARS = string.ascii_letters + string.digits
+
+    global counter
+    unique_id = counter
+    num = unique_id
+    counter += 1
+
+    if num == 0:
+        return BASE62_CHARS[0]
+
+    encoded = ""
+    while num > 0:
+        num, rem = divmod(num, BASE)
+        encoded = BASE62_CHARS[int(rem)] + encoded
+
+    url_dict[url] = encoded
+
+    return f"{BASE_URL}/{encoded}"
--- test_shortner.py
+++ test_shortner.py
@@ -1,11 +1,30 @@
-from functions import url_shortner_md5, url_shortner_alphanumeric, URL_LEN
+import pytest
+from functions import (
+    url_shortner_md5,
+    url_shortner_alphanumeric,
+    URL_LEN,
+    url_shortner_by_base_conversion,
+)
 
 
-def test_url_shortner_md5():
-    test_url1 = "www.google.com"
+@pytest.fixture
+def test_url() -> str:
+    return "www.google.com"
 
-    shortned_url1 = url_shortner_md5(url=test_url1)
-    shortned_url2 = url_shortner_md5(url=test_url1)
+
+def test_url_shortner_md5(test_url: str):
+    shortned_url1 = url_shortner_md5(url=test_url)
+    shortned_url2 = url_shortner_md5(url=test_url)
+
+    assert shortned_url1 == shortned_url2
+
+    short_url_code = shortned_url1.rsplit("/", 1)[-1]
+    assert len(short_url_code) == URL_LEN
+
+
+def test_url_shortner_alphanumeric(test_url: str):
+    shortned_url1 = url_shortner_alphanumeric(url=test_url)
+    shortned_url2 = url_shortner_alphanumeric(url=test_url)
 
     assert shortned_url1 == shortned_url2
 
@@ -13,13 +32,8 @@ def test_url_shortner_md5():
     assert len(short_url_code) == URL_LEN
 
 
-def test_url_shortner_alphanumeric():
-    test_url1 = "www.google.com"
-
-    shortned_url1 = url_shortner_alphanumeric(url=test_url1)
-    shortned_url2 = url_shortner_alphanumeric(url=test_url1)
+def test_url_shortner_by_base_conversion(test_url: str):
+    shortned_url1 = url_shortner_by_base_conversion(url=test_url)
+    shortned_url2 = url_shortner_by_base_conversion(url=test_url)
 
     assert shortned_url1 == shortned_url2
-
-    short_url_code = shortned_url1.rsplit("/", 1)[-1]
-    assert len(short_url_code) == URL_LEN
